9641a3f94b035908766e6ee48eb142dca98e5246
Jakub <Jakub@MacBook-Pro-mac.local>
2021-07-13 14:05:23 +0200

    little vistial effects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,17 @@
 import React from "react";
+import {Link } from 'gatsby'
 import Navbar from "./Navbar";
 
 export default function Footer({ data }) {
   return (
     <footer className="footer">
-      <div className="header__logo header__logo--white">
+      <Link to='/' className="header__logo header__logo--white">
         <em className="logo__firstLetter logo__firstLetter--white">Ż</em>
         <h1>
           ŻYCIE <br />
           OD NOWA
         </h1>
-      </div>
+      </Link>
       <Navbar data={data} location={"footer"} />
       <div className="footer__decoration"></div>
     </footer>
