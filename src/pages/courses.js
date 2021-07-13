391cd0b4eb5ad3d29a7b1f59ce1b6e16d29b77df
Jakub <Jakub@MacBook-Pro-mac.local>
2021-07-13 13:17:36 +0200

    fixing layout

---
+++ src/pages/courses.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+
+export default function Courses() {
+    return (
+        <Layout>
+            <h1>Courses</h1>
+        </Layout>
+    )
+}
